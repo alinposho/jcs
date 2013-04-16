8886430960b33fcd99c852c093df45a08ae4b12e
aposorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-04-16 12:00:02 +0300

    Add a test for MAX_IDLE_TIMEOUT - the test doesn't work.

--- src/test/java/org/jcs/sample/code/BasicJcsConfigTest.java
+++ src/test/java/org/jcs/sample/code/BasicJcsConfigTest.java
@@ -1,37 +1,57 @@
 package org.jcs.sample.code;
 
 
+import junit.framework.Assert;
 import org.apache.jcs.JCS;
 import org.apache.jcs.access.exception.CacheException;
 import org.apache.jcs.engine.behavior.IElementAttributes;
 import org.apache.jcs.engine.control.event.behavior.IElementEvent;
+import org.apache.jcs.engine.control.event.behavior.IElementEventConstants;
 import org.apache.jcs.engine.control.event.behavior.IElementEventHandler;
 import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
 
 public class BasicJcsConfigTest {
 
 
+    public static final String TEST_CACHE_REGION = "testCache1";
+    private static final int MAX_MEMORY_IDLE_TIME = 5;
+
     @Test
     public void testAddListener() throws CacheException {
 
-        JCS jcs = JCS.getInstance("testCache1");
-        MyEventHandler meh = new MyEventHandler();
-
-        // jcs.getDefaultElementAttributes returns a copy not a reference
-        IElementAttributes attributes = jcs.getDefaultElementAttributes();
-        attributes.addElementEventHandler(meh);
-        jcs.put("key", "data", attributes);
+        JCS jcs = JCS.getInstance(TEST_CACHE_REGION);
+        putElementIntoCache(jcs);
 
         jcs.freeMemoryElements(1);
-        jcs.clear();
     }
 
-}
-
-class MyEventHandler implements IElementEventHandler {
-
-    @Override
-    public void handleElementEvent(IElementEvent event) {
-        System.out.println("Received event: " + event.getElementEvent());
+    private void putElementIntoCache(JCS jcs) throws CacheException {
+        IElementAttributes attributes = jcs.getDefaultElementAttributes();
+        attributes.addElementEventHandler(createEventHandlerForSpolledDiskNotAvailable());
+        jcs.put("key", "data", attributes);
     }
+
+    private IElementEventHandler createEventHandlerForSpolledDiskNotAvailable() {
+        return new IElementEventHandler() {
+            @Override
+            public void handleElementEvent(IElementEvent iElementEvent) {
+                assertEquals(IElementEventConstants.ELEMENT_EVENT_SPOOLED_DISK_NOT_AVAILABLE, iElementEvent.getElementEvent());
+                System.out.println("Event received " + iElementEvent.getElementEvent());
+            }
+        };
+    }
+
+    @Test
+    public void should_fire_the_event_listener_when_evicting_oject_from_cache() throws CacheException, InterruptedException {
+
+        JCS jcs = JCS.getInstance(TEST_CACHE_REGION);
+        putElementIntoCache(jcs);
+
+        Thread.sleep((MAX_MEMORY_IDLE_TIME + 1) * 1000);
+
+    }
+
 }
