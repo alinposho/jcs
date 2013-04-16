09a834d868a12c83b47f106fa34486caa3212fd5
aposorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-04-16 11:22:18 +0300

    Manually add jcs and its dependencies to the project.

--- src/test/java/org/jcs/sample/code/BasicJcsConfigTest.java
+++ src/test/java/org/jcs/sample/code/BasicJcsConfigTest.java
@@ -1,4 +1,39 @@
 package org.jcs.sample.code;
 
+
+import org.apache.jcs.JCS;
+import org.apache.jcs.access.exception.CacheException;
+import org.apache.jcs.engine.behavior.IElementAttributes;
+import org.apache.jcs.engine.control.event.behavior.IElementEvent;
+import org.apache.jcs.engine.control.event.behavior.IElementEventHandler;
+import org.junit.Test;
+
 public class BasicJcsConfigTest {
+
+
+    @Test
+    public void testAddListener() throws CacheException {
+
+        JCS jcs = JCS.getInstance("testCache1");
+
+        MyEventHandler meh = new MyEventHandler();
+
+        // jcs.getDefaultElementAttributes returns a copy not a reference
+        IElementAttributes attributes = jcs.getDefaultElementAttributes();
+        attributes.addElementEventHandler(meh);
+        jcs.put("key", "data", attributes);
+
+        jcs.freeMemoryElements(1);
+//     jcs.remove("key");
+        jcs.clear();
+    }
+
+}
+
+class MyEventHandler implements IElementEventHandler {
+
+    @Override
+    public void handleElementEvent(IElementEvent event) {
+        System.out.println("Received event: " + event.getElementEvent());
+    }
 }
